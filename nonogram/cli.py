33612920efc4ea996060809a307b8ed47bc9b36d
Arthur Chang <arthurc0102@gmail.com>
2024-04-04 13:16:59 +0800

    feat: add line-by-line option

--- nonogram/cli.py
+++ nonogram/cli.py
@@ -9,7 +9,10 @@ app = typer.Typer()
 
 
 @app.command()
-def solve(file: typing.Annotated[pathlib.Path, typer.Argument()]) -> None:
+def solve(
+    file: typing.Annotated[pathlib.Path, typer.Argument()],
+    line_by_line: typing.Annotated[bool, typer.Option()] = False,  # noqa: FBT002
+) -> None:
     length, _, *info = file.read_text().splitlines()
 
     length = int(length)
@@ -17,4 +20,18 @@ def solve(file: typing.Annotated[pathlib.Path, typer.Argument()]) -> None:
     row_info = [[int(n) for n in i.split()] for i in info[length + 1 :]]
 
     solver = services.NonogramSolver(col_info, row_info)
-    typer.echo(solver.answer_str)
+    if not line_by_line:
+        typer.echo(solver.answer_str)
+        return
+
+    typer.echo(" ".join(("1", "2", "3", "4", "5") * (length // 5)))
+    typer.echo("-" * (length * 2 - 1))
+
+    lines = solver.answer_str.splitlines()
+    for idx, line in enumerate(lines, 1):
+        typer.echo(line, nl=False)
+
+        if idx != len(lines):
+            input()
+        else:
+            typer.echo()
